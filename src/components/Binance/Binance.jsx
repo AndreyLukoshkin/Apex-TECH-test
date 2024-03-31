0a830222157ef1c9cf9258bc48eff96a551b9adc
Andrey Lukoshkin <lukoshkin.andrey@gmail.com>
2024-03-31 05:03:43 +0300

    "Created a chart using the Apex Charts library. The chart updates based on the selected time interval, showing the current price for the BTCUST pair

--- src/components/Binance/Binance.jsx
+++ src/components/Binance/Binance.jsx
@@ -1,19 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './styles.css'
-import Graphic from '../Graphic/Graphic'
+// import Graphic from '../Graphic/Graphic'
 import TimePeriod from '../TimePeriod/TimePeriod'
 import Price from '../Price/Price'
+import CandlestickChart from '../CandlestickChart/CandlestickChart'
+import axios from 'axios'
 
 const Binance = () => {
+  const [time, setTime] = useState('1h')
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    const fetchCandleData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=${time}`
+        )
+        setData(response)
+      } catch (error) {
+        console.error('Error fetching candle data:', error)
+      }
+    }
+
+    fetchCandleData()
+  }, [time])
+
   return (
     <div className="binance__container">
       <div className="binance__wrapper">
-        <div className="binance__wrapper_top">
-          <TimePeriod />
+        <div className="binance__wrapper_top-part">
+          <TimePeriod time={time} setTime={setTime} />
           <Price />
         </div>
-        <Graphic />
+        <div className="binance__wrapper_bottom-part">
+          {data && <CandlestickChart data={data.data} />}
+        </div>
       </div>
     </div>
   )
